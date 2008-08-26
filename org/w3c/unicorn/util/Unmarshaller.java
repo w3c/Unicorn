0b09a4414e1feb31ff401626754f7b321095b058
fbatard <fbatard@localhost>
2008-08-26 15:28:28 +0000

    Update for the unmarshaller

--- org/w3c/unicorn/util/Unmarshaller.java
+++ org/w3c/unicorn/util/Unmarshaller.java
@@ -1,4 +1,4 @@
-// $Id: Unmarshaller.java,v 1.2 2008-06-17 13:45:31 jbarouh Exp $
+// $Id: Unmarshaller.java,v 1.3 2008-08-26 15:28:28 fbatard Exp $
 // Author: Damien LEROY.
 // (c) COPYRIGHT MIT, ERCIM ant Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
@@ -7,17 +7,13 @@ package org.w3c.unicorn.util;
 import java.io.IOException;
 import java.net.URL;
 
-import javax.xml.bind.JAXBException;
-
-import org.xml.sax.SAXException;
-
 /**
  * Interface for all unmarshaller class in package unicorn.
  * @author Damien LEROY
  */
 public interface Unmarshaller {
 
-	public void addURL (final URL aURL) throws IOException, JAXBException, SAXException;
+	public void addURL (final URL aURL) throws IOException;
 
 	public void unmarshal () throws Exception;
 
