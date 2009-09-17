f7ad2b8055c28a4e4696e798f53e2d538688b795
tgambet <tgambet@localhost>
2009-09-17 15:36:20 +0000

    new exception. can contain a Message object

---
+++ src/org/w3c/unicorn/exceptions/UnicornException.java
@@ -0,0 +1,27 @@
+package org.w3c.unicorn.exceptions;
+
+import org.w3c.unicorn.util.Message;
+
+public class UnicornException extends Exception {
+	
+	private static final long serialVersionUID = 3302368429707755988L;
+
+	private Message message;
+	
+	public UnicornException(String string) {
+		super(string);
+	}
+	
+	public UnicornException(Message message) {
+		this.message = message;
+	}
+
+	public Message getUnicornMessage() {
+		return message;
+	}
+
+	public void setUnicornMessage(Message message) {
+		this.message = message;
+	}
+	
+}
