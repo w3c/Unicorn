b43a537d164c5a161c771f99ab83792a0de16ff1
tgambet <tgambet@localhost>
2009-09-17 10:36:43 +0000

    test connection exceptions

--- src/org/w3c/unicorn/Test.java
+++ src/org/w3c/unicorn/Test.java
@@ -1,11 +1,26 @@
 package org.w3c.unicorn;
 
-import java.util.Locale;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.ConnectException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.UnknownHostException;
+
+import javax.net.ssl.SSLException;
+
+//import sun.net.www.protocol.http.HttpURLConnection;
+
+
 
 public class Test {
 
 	/**
 	 * @param args
+	 * @throws MalformedURLException 
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -26,9 +41,52 @@ public class Test {
 		System.out.println(matcher.group());
 	     //replaceAll(repl)*/
 		
-		Locale loc = new Locale("sssqn");
-		
-		System.out.println("z" + loc.getLanguage());
+		URL docUrl = null;
+		try {
+			docUrl = new URL("http://www.w3.org:9788");
+			//docUrl = new URL("http://qa-dev.w3.org/app");
+			//docUrl = new URL("http://www.w3.org/Team");
+			//docUrl = new URL("http://snowball.w3.org/unicorn/observe?ucn_uri=beta.w3.org&ucn_task=conformance");
+			//System.out.println(docUrl.getContent().toString());
+			//System.out.println(docUrl2.getContent().toString().length());
+			URLConnection con = docUrl.openConnection();
+			System.out.println(con.getConnectTimeout());
+			con.setConnectTimeout(1000);
+			con.setReadTimeout(1000);
+			con.getContent();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (ConnectException e) {
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (UnknownHostException e) { 
+			e.printStackTrace();
+		} catch (SSLException e) {
+			e.printStackTrace();
+		} catch (SocketTimeoutException e) {
+			e.printStackTrace();
+			if (e.getMessage().contains("Read timed out"))
+				System.out.println("a");
+			if (e.getMessage().contains("connect timed out"))
+				System.out.println("b");
+		} catch (IOException e) {
+			try {
+				int responseCode;
+				if (docUrl != null) {
+					responseCode = ((HttpURLConnection) docUrl.openConnection()).getResponseCode();
+					switch (responseCode) {
+					case HttpURLConnection.HTTP_UNAUTHORIZED:
+						System.out.println("HTTP_UNAUTHORIZED");
+						break;
+					}
+				}
+			} catch (Exception e2) {
+				System.out.println("e2");
+				e2.printStackTrace();
+			}
+			e.printStackTrace();
+		}
 	}
 
 }
