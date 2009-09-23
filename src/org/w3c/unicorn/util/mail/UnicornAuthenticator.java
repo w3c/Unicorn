08b9daa6a1339d40b70045043ae934dfe76c6871
tgambet <tgambet@localhost>
2009-09-23 12:56:49 +0000

    Authenticator that manage smtp username and password

---
+++ src/org/w3c/unicorn/util/mail/UnicornAuthenticator.java
@@ -0,0 +1,39 @@
+package org.w3c.unicorn.util.mail;
+
+import java.awt.HeadlessException;
+import java.util.StringTokenizer;
+
+import javax.mail.Authenticator;
+import javax.mail.PasswordAuthentication;
+import javax.swing.JOptionPane;
+
+import org.w3c.unicorn.Framework;
+
+public class UnicornAuthenticator extends Authenticator {
+
+	String username;
+	String password;
+	
+	public UnicornAuthenticator(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+	
+	@Override
+	protected PasswordAuthentication getPasswordAuthentication() {
+
+		try {
+			if (username == null && password == null) {
+			    String result = JOptionPane.showInputDialog("Enter 'username,password' for your SMTP Server. This is asked only once. You can also set the username and password in mail.properties");
+			    StringTokenizer st = new StringTokenizer(result, ",");
+			    username = st.nextToken();
+			    password = st.nextToken();
+			}
+		} catch (HeadlessException e) {
+			Framework.logger.error("Before sending mails you must specify a username and a password in mail.properties. If your SMTP server does not need authentication, set mail.smtp.auth to false.");
+		}
+
+	    return new PasswordAuthentication(username, password);
+	}
+	
+}
