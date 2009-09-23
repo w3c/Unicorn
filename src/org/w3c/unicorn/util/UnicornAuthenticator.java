dc06f66cb2740b976dbcc3019b066b05311e1863
tgambet <tgambet@localhost>
2009-09-23 15:35:40 +0000

    commented JOptionPane.showInputDialog

--- src/org/w3c/unicorn/util/UnicornAuthenticator.java
+++ src/org/w3c/unicorn/util/UnicornAuthenticator.java
@@ -1,11 +1,7 @@
 package org.w3c.unicorn.util;
 
-import java.awt.HeadlessException;
-import java.util.StringTokenizer;
-
 import javax.mail.Authenticator;
 import javax.mail.PasswordAuthentication;
-import javax.swing.JOptionPane;
 
 import org.w3c.unicorn.Framework;
 
@@ -24,12 +20,13 @@ public class UnicornAuthenticator extends Authenticator {
 
 		try {
 			if (username == null && password == null) {
-			    String result = JOptionPane.showInputDialog("Enter 'username,password' for your SMTP Server. This is asked only once. You can also set the username and password in mail.properties");
+				/*String result = JOptionPane.showInputDialog("Enter 'username,password' for your SMTP Server. This is asked only once. You can also set the username and password in mail.properties");
 			    StringTokenizer st = new StringTokenizer(result, ",");
 			    username = st.nextToken();
-			    password = st.nextToken();
+			    password = st.nextToken();*/
+				throw new Exception();
 			}
-		} catch (Exception e) {
+		} catch (Exception e) { //catch (HeadlessException e) {
 			Framework.logger.error("Before sending mails you must specify a username and a password in mail.properties. If your SMTP server does not need authentication, set mail.smtp.auth to false.");
 		}
 
