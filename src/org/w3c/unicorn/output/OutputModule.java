1fa9f90573d2346994cb9ea66dd815bc78d61e5b
tgambet <tgambet@localhost>
2009-09-24 15:28:42 +0000

    OutputModule is now an abstract class + implements displayOnIndex method that replace redirection to index

--- src/org/w3c/unicorn/output/OutputModule.java
+++ src/org/w3c/unicorn/output/OutputModule.java
@@ -1,23 +1,36 @@
-// $Id: OutputModule.java,v 1.8 2009-09-23 09:26:05 tgambet Exp $
+// $Id: OutputModule.java,v 1.9 2009-09-24 15:28:42 tgambet Exp $
 // Author: Damien LEROY.
 // (c) COPYRIGHT MIT, ERCIM ant Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.unicorn.output;
 
+import java.io.IOException;
 import java.io.Writer;
 import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.velocity.VelocityContext;
+import org.w3c.unicorn.util.Language;
+import org.w3c.unicorn.util.Templates;
 
 /**
  * Interface for all output module.
  * 
  * @author Damien LEROY
  */
-public interface OutputModule {
-
+public abstract class OutputModule {
+	
+	protected Map<String, String> outputParameters;
+	
+	protected Map<String, String> specificParameters;
+	
 	public static final Log logger = LogFactory.getLog(OutputModule.class);
+	
+	public OutputModule(Map<String, String> mapOfOutputParameters, Map<String, String> mapOfSpecificParameters) {
+		this.outputParameters = mapOfOutputParameters;
+		this.specificParameters = mapOfSpecificParameters;
+	}
 	
 	/**
 	 * Generate a first output before the requests are launched.
@@ -37,6 +50,30 @@ public interface OutputModule {
 	 */
 	public abstract void produceError(Map<String, Object> mapOfStringObject, final Writer aWriter);
 
-	public abstract String getOutputParameter(String string);
+	protected void displayOnIndex(Map<String, Object> mapOfStringObject, Writer writer) {
+		VelocityContext context = new VelocityContext(Language.getContext(outputParameters.get("lang")));
+		
+		for (final String sObjectName : mapOfStringObject.keySet())
+			context.put(sObjectName, mapOfStringObject.get(sObjectName));
+		
+		Templates.write("index.vm", context, writer);
+		try {
+			writer.close();
+		} catch (IOException e) {
+			logger.error("Error closing output writer: " + e.getMessage(), e);
+		}
+	}
+	
+	public String getOutputParameter(String string) {
+		return outputParameters.get(string);
+	}
+	
+	public Map<String, String> getOutputParameters() {
+		return outputParameters;
+	}
 
+	public void setOutputParameters(Map<String, String> mapOfOutputParameters) {
+		this.outputParameters = mapOfOutputParameters;
+	}
+	
 }
