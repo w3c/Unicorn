5de55ca875c28f09f7215372ad382355a537a849
tgambet <tgambet@localhost>
2009-09-24 15:30:49 +0000

    produceError displays on index if the mimeType is text/html.

--- src/org/w3c/unicorn/output/SimpleOutputModule.java
+++ src/org/w3c/unicorn/output/SimpleOutputModule.java
@@ -1,4 +1,4 @@
-// $Id: SimpleOutputModule.java,v 1.7 2009-09-23 09:26:05 tgambet Exp $
+// $Id: SimpleOutputModule.java,v 1.8 2009-09-24 15:30:49 tgambet Exp $
 // Author: Damien LEROY.
 // (c) COPYRIGHT MIT, ERCIM ant Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
@@ -6,6 +6,7 @@ package org.w3c.unicorn.output;
 
 import java.io.Writer;
 import java.util.Map;
+
 import org.w3c.unicorn.output.OutputFactory;
 
 /**
@@ -13,14 +14,13 @@ import org.w3c.unicorn.output.OutputFactory;
  * 
  * @author Damien LEROY
  */
-public class SimpleOutputModule implements OutputModule {
+public class SimpleOutputModule extends OutputModule {
 
 	private  OutputFormater aOutputFormater;
 	
-	private Map<String, String> mapOfOutputParameters;
-	
 	public SimpleOutputModule(Map<String, String> mapOfOutputParameters, Map<String, String> mapOfSpecificParameters) {
-		this.mapOfOutputParameters = mapOfOutputParameters;
+		super(mapOfOutputParameters, mapOfSpecificParameters);
+		
 		aOutputFormater = OutputFactory.createOutputFormater(mapOfOutputParameters.get("format"),
 				mapOfOutputParameters.get("lang"), mapOfOutputParameters.get("mimetype"));
 	}
@@ -30,17 +30,15 @@ public class SimpleOutputModule implements OutputModule {
 	}
 
 	public void produceError(Map<String, Object> mapOfStringObject, final Writer aWriter) {
+		if (outputParameters.get("mimetype").equals("text/html")) {
+			displayOnIndex(mapOfStringObject, aWriter);
+			return;
+		}
 		aOutputFormater.produceError(mapOfStringObject, aWriter);
 	}
 
 	public void produceFirstOutput(Map<String, Object> mapOfStringObject, Writer aWriter) {
 		return;
 	}
-
-	public String getOutputParameter(String string) {
-		return mapOfOutputParameters.get(string);
-	}
 	
-	
-
 }
