3ff506bfb4775538eec39e0f2730f3ec7a836089
tgambet <tgambet@localhost>
2009-09-29 16:07:57 +0000

    refactored with new OutputModule class

--- src/org/w3c/unicorn/output/SimpleOutputModule.java
+++ src/org/w3c/unicorn/output/SimpleOutputModule.java
@@ -1,4 +1,4 @@
-// $Id: SimpleOutputModule.java,v 1.8 2009-09-24 15:30:49 tgambet Exp $
+// $Id: SimpleOutputModule.java,v 1.9 2009-09-29 16:07:57 tgambet Exp $
 // Author: Damien LEROY.
 // (c) COPYRIGHT MIT, ERCIM ant Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
@@ -7,38 +7,27 @@ package org.w3c.unicorn.output;
 import java.io.Writer;
 import java.util.Map;
 
-import org.w3c.unicorn.output.OutputFactory;
-
 /**
  * This module allow to generate output in text format.
  * 
  * @author Damien LEROY
  */
 public class SimpleOutputModule extends OutputModule {
-
-	private  OutputFormater aOutputFormater;
 	
 	public SimpleOutputModule(Map<String, String> mapOfOutputParameters, Map<String, String> mapOfSpecificParameters) {
 		super(mapOfOutputParameters, mapOfSpecificParameters);
-		
-		aOutputFormater = OutputFactory.createOutputFormater(mapOfOutputParameters.get("format"),
-				mapOfOutputParameters.get("lang"), mapOfOutputParameters.get("mimetype"));
 	}
 	
 	public void produceOutput(Map<String, Object> mapOfStringObject, final Writer aWriter) {
-		aOutputFormater.produceOutput(mapOfStringObject, aWriter);
+		defaultOutputFormater.produceOutput(mapOfStringObject, aWriter);
 	}
 
 	public void produceError(Map<String, Object> mapOfStringObject, final Writer aWriter) {
-		if (outputParameters.get("mimetype").equals("text/html")) {
+		if (getMimeType().equals("text/html")) {
 			displayOnIndex(mapOfStringObject, aWriter);
 			return;
 		}
-		aOutputFormater.produceError(mapOfStringObject, aWriter);
-	}
-
-	public void produceFirstOutput(Map<String, Object> mapOfStringObject, Writer aWriter) {
-		return;
+		defaultOutputFormater.produceError(mapOfStringObject, aWriter);
 	}
 	
 }
