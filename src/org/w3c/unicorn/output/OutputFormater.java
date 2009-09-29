aefa8def6b93fca26a6cea2e0e01c9b5f27d593f
tgambet <tgambet@localhost>
2009-09-29 16:03:31 +0000

    changed to abstract class + added lang, format, mimetype variables

--- src/org/w3c/unicorn/output/OutputFormater.java
+++ src/org/w3c/unicorn/output/OutputFormater.java
@@ -1,4 +1,4 @@
-// $Id: OutputFormater.java,v 1.6 2009-09-21 15:50:19 tgambet Exp $
+// $Id: OutputFormater.java,v 1.7 2009-09-29 16:03:31 tgambet Exp $
 // Author: Jean-Guilhem Rouel
 // (c) COPYRIGHT MIT, ERCIM and Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
@@ -9,6 +9,7 @@ import java.util.Map;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.w3c.unicorn.util.Property;
 
 /**
  * SimpleOutputFormater<br />
@@ -16,8 +17,24 @@ import org.apache.commons.logging.LogFactory;
  * 
  * @author Jean-Guilhem Rouel
  */
-public interface OutputFormater {
+public abstract class OutputFormater {
 
+	protected String mimeType;
+	
+	protected String format;
+	
+	protected String lang;
+	
+	public OutputFormater(String format, String lang) {
+		logger.trace("Constructor");
+		logger.debug("Output format : " + format + ".");
+		logger.debug("Output language : " + lang + ".");
+		
+		this.mimeType = Property.getProps("output.properties").getProperty(format + ".mimetype");
+		this.format = format;
+		this.lang = lang;
+	}
+	
 	public static final Log logger = LogFactory.getLog(OutputFormater.class);
 	
 	/**
@@ -30,6 +47,18 @@ public interface OutputFormater {
 	 * @param messages 
 	 * @param aWriter
 	 */
-	public abstract void produceError(Map<String, Object> mapOfStringObject, Writer output);
+	public abstract void produceError(Map<String, Object> mapOfStringObject, Writer output);	
+
+	public String getMimeType() {
+		return mimeType;
+	}
+	
+	public String getLang() {
+		return lang;
+	}
+
+	public String getFormat() {
+		return format;
+	}
 
 }
