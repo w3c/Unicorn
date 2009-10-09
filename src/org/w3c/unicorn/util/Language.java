1d63e318b5f0a481fbf84c169b6dd0d6471e7cb0
tgambet <tgambet@localhost>
2009-10-09 15:00:45 +0000

    adds 'dir' and 'xml:lang' attributes on untranslated strings

--- src/org/w3c/unicorn/util/Language.java
+++ src/org/w3c/unicorn/util/Language.java
@@ -58,7 +58,7 @@ public class Language {
 		for (Object key : defaultProps.keySet()) {
 			if (!props.containsKey(key)) {
 				props.put("complete", "false");
-				props.put(key, defaultProps.get(key));
+				props.put(key, "<span dir=\"" + defaultProps.get("direction") + "\" xml:lang=\"" + defaultProps.get("lang") + "\">" + defaultProps.get(key) + "</span>");
 				Framework.logger.warn(">> Missing property in " + props.getProperty("lang") + ".properties for key: \"" + (String) key + "\". Added default property for this key: \"" + defaultProps.get(key) + "\""); 
 			}
 		}
