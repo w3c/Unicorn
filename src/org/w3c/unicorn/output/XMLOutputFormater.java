25f56be3c7eaf23989e516c6b5016a4c5ed7b74e
tgambet <tgambet@localhost>
2009-10-22 14:59:51 +0000

    XHTMLize is not needed anymore, escaping is done inside the templates only where needed with Escape Velocity Tool

--- src/org/w3c/unicorn/output/XMLOutputFormater.java
+++ src/org/w3c/unicorn/output/XMLOutputFormater.java
@@ -1,11 +1,8 @@
-// $Id: XMLOutputFormater.java,v 1.8 2009-10-09 06:46:40 tgambet Exp $
+// $Id: XMLOutputFormater.java,v 1.9 2009-10-22 14:59:51 tgambet Exp $
 // Author: Damien LEROY.
 // (c) COPYRIGHT MIT, ERCIM ant Keio, 2006.
 // Please first read the full copyright statement in file COPYRIGHT.html
 package org.w3c.unicorn.output;
-
-import org.apache.velocity.app.event.EventCartridge;
-import org.w3c.unicorn.util.XHTMLize;
 
 /**
  * Class for XML output formater.
@@ -19,9 +16,9 @@ public class XMLOutputFormater extends SimpleOutputFormater {
 		super(format, lang, mimeType);
 		
 		// Replace tag objects (A, Img, ...) with their XHTML representation
-		final EventCartridge aEventCartridge = new EventCartridge();
-		aEventCartridge.addEventHandler(new XHTMLize());
-		aEventCartridge.attachToContext(aVelocityContext);
+		//final EventCartridge aEventCartridge = new EventCartridge();
+		//aEventCartridge.addEventHandler(new XHTMLize());
+		//aEventCartridge.attachToContext(aVelocityContext);
 	}
 
 }
--- src/org/w3c/unicorn/util/XHTMLize.java
+++ src/org/w3c/unicorn/util/XHTMLize.java
@@ -1,24 +1,7 @@
 package org.w3c.unicorn.util;
 
-import org.apache.commons.lang.StringEscapeUtils;
 import org.apache.velocity.app.event.ReferenceInsertionEventHandler;
 
-/**
- * Escape all XML Entities in the reference insertion. Specifically, the
- * following conversions are performed:
- * <DL>
- * <DT>&amp;</DT>
- * <DD>&amp;amp;</DD>
- * <DT>&lt;</DT>
- * <DD>&amp;lt;</DD>
- * <DT>&gt;</DT>
- * <DD>&amp;gt;</DD>
- * <DT>&quot;</DT>
- * <DD>&amp;quot;</DD>
- * </DL>
- * 
- * @author <a href="mailto:wglass@forio.com">Will Glass-Husain</a>
- */
 public class XHTMLize implements ReferenceInsertionEventHandler {
 
 	/**
@@ -28,19 +11,6 @@ public class XHTMLize implements ReferenceInsertionEventHandler {
 		if (oValue == null) {
 			return null;
 		}
-/*		if (oValue instanceof A) {
-			A link = (A) oValue;
-			return insertA(link);
-		}
-		if (oValue instanceof Img) {
-			Img image = (Img) oValue;
-			return insertImg(image);
-		}
-		if (oValue instanceof Code) {
-			Code code = (Code) oValue;
-			return insertCode(code);
-		}*/
-		
 		return oValue.toString();
 		
 		/*if (sReference.startsWith("$ucn.evaluate"))
@@ -50,61 +20,5 @@ public class XHTMLize implements ReferenceInsertionEventHandler {
 		else
 			return StringEscapeUtils.escapeHtml(oValue.toString());*/
 	}
-
-	/**
-	 * Insert a link
-	 * 
-	 * @param aLink
-	 *            link to insert
-	 * @return return the object containing the link
-	 */
-/*	private Object insertA(final A aLink) {
-		String sResultat = "<a href=\""
-			    + StringEscapeUtils.escapeHtml(aLink.getHref()) + "\">";
-		for (final Object oElement : aLink.getContent()) {
-			if (oElement instanceof Img) {
-				sResultat += insertImg((Img) oElement);
-			} else {
-				sResultat += StringEscapeUtils.escapeHtml(oElement.toString());
-			}
-		}
-		sResultat += "</a>";
-		return sResultat;
-	}
-
-	/**
-	 * Insert code tag into the tags
-	 * 
-	 * @param aCode
-	 *            code to insert
-	 * @return object with code inserted
-	 */
-/*	private Object insertCode(final Code aCode) {
-		String sResultat = "<code>";
-		for (final Object oElement : aCode.getContent()) {
-			if (oElement instanceof A) {
-				sResultat += insertA((A) oElement);
-			} else if (oElement instanceof Img) {
-				sResultat += insertImg((Img) oElement);
-			} else {
-				sResultat += StringEscapeUtils.escapeHtml(oElement.toString());
-			}
-		}
-		sResultat += "</code>";
-		return sResultat;
-	}
-
-	/**
-	 * Insert an img tag
-	 * 
-	 * @param img
-	 *            image path to insert
-	 * @return the string containing the image tag
-	 */
-/*	private String insertImg(final Img aImage) {
-		return "<img src=\"" + StringEscapeUtils.escapeHtml(aImage.getSrc())
-				+ "\" alt=\"" + StringEscapeUtils.escapeHtml(aImage.getAlt())
-				+ "\"/>";
-	}*/
 
 }
